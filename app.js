5ecf68b07c06646b2c2cdfbca2dd151c2f45c82d
aisom4 <128502068+aisom4@users.noreply.github.com>
2023-05-11 08:40:03 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 window.onload = function () {
-  fetch("products.json")
-    .then((response) => response.json())
+  //fetches the data from the json page when the page is done loading
+  fetch("products.json") //Initiates the fetching and data processing
+    .then((response) => response.json()) //returns result of parsed JSON file
     .then((data) => {
+      //data is a parameter used to loop through the JSON array
       console.log(data);
-      let productHTML = "";
+      let productHTML = ""; //Sets an empty spot for the HTML code for each of the products on the page.
 
       for (let i = 0; i < data.length; i++) {
+        // a For Loop that
         let item = data[i];
-
+        //After the loop the generated code will flow into the productHTML variable we will access the data using template literals for each item
         let product = `<div class="seller">
           
 
           <span class="item-row-2">
-            <h2>${item.name}</h2>
+            <h2>${item.name}</h2> 
           </span>
 
           <span class="item-row-3">
@@ -29,44 +32,52 @@ window.onload = function () {
           </span>
         </div>
         `;
-        productHTML += product;
+        productHTML += product; //all generated code is added to the productHTML string to overwrite and display properly
       }
 
-      let bestsellers = document.getElementsByClassName("best-sellers")[0];
+      let bestsellers = document.getElementsByClassName("best-sellers")[0]; // taking the items fromt he HTML class "best-sellers"
       bestsellers.innerHTML = productHTML;
-    });
+    }); // assignment of the innerHTML property which updates the products on the actal page.
 };
 
-let slideIndex = 1;
-showSlides(slideIndex);
+//Navagating through image slides on Home Page
+
+let slideIndex = 1; //first variable is initialized to 1 to show which slide to display first
+showSlides(slideIndex); //calling the future function "showSlides" and passing slideIndex variable as an argument
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides((slideIndex += n));
+  //--Used along with currentSlide function to change the current slide by altering the slideIndex variable
+  showSlides((slideIndex += n)); //increments the slideIndex based on the parameter n being passed
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
+  // creating a function with the same n parameter to show the proper corresponding slides when pressing the dot
   showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("homeSlides");
-  let dots = document.getElementsByClassName("dot");
+  //taking the parameter n another time n-represents what slide is going to display. --Handles the actual slide display and the active dot display.
+  let i; // Initializes i as undefined
+  let slides = document.getElementsByClassName("homeSlides"); // letting the slides equal the class on the html page.
+  let dots = document.getElementsByClassName("dot"); // calling the dot class from the index.html page
   if (n > slides.length) {
+    //if our n variable is greater than the total number of slides the slideIndex will show the first slide
     slideIndex = 1;
   }
   if (n < 1) {
+    // if it is less than 1 the slideIndex will be set to show the last slide. This helps bring the slides around if you are clicking through the photos backwards
     slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
+    //creates another loop and changes the display style to noen to hide the elements
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", ""); //.replace method is removing the active class from the dot that is no longer clicked
   }
-  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block"; //display style is set to block and active class is added to update the dots on the bottom of the images
   dots[slideIndex - 1].className += " active";
 }
 
